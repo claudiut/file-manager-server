717572f3cfbca8ef0d177f68abf1dc90653b01fc
claudiut <claudiut@users.noreply.github.com>
2021-03-04 10:11:30 +0200

    Add rename ability

--- index.js
+++ index.js
@@ -4,68 +4,102 @@ const path = require('path');
 const app = express();
 const mime = require('mime');
 const fileUpload = require('express-fileupload');
+const bodyParser = require('body-parser');
 
 const port = 4444;
-const basePath = path.resolve('../../Documents/File Browser Test');
+const basePath = path.resolve('../../Documents/FileManagerTest');
 
+// Middleware
+// CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
+    res.header("Access-Control-Allow-Methods", "DELETE,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(fileUpload());
+app.use(bodyParser.json());
+
+// Helpers
 const filterVisibleFiles = filePath => !filePath.startsWith('.');
 
 const isDir = async filePath => (await fs.lstat(filePath)).isDirectory()
 
 const getFullPath = subPath => path.join(basePath, subPath)
 
-const readDirFiles = async (subPath, depth) => {
-    const dirPath = getFullPath(subPath);
-    if (!await isDir(dirPath)) {
+const getFileOrDirInfo = (subPath) => {
+    const diskPath = getFullPath(subPath);
+    return fs.lstat(diskPath).then(stats => {
+        const isDir = stats.isDirectory();
+        const isFile = stats.isFile();
+        if (!isDir && !isFile) {
+            return null;
+        }
+
+        return {
+            path: subPath,
+            isDir,
+            mimeType: mime.getType(diskPath),
+        };
+    });
+};
+
+const readDirFiles = async (subDirPath, depth) => {
+    const fullDirPath = getFullPath(subDirPath);
+    if (!await isDir(fullDirPath)) {
         return null;
     }
 
-    const files = (await fs.readdir(dirPath)).filter(filterVisibleFiles);
-    const stats = await Promise.all(files.map((fPath) => fs.lstat(path.join(dirPath, fPath))))
+    const files = (await fs.readdir(fullDirPath)).filter(filterVisibleFiles);
 
-    const filesWithStats = files
-        .map((filename, i) => {
-            const isDir = stats[i].isDirectory();
-            const isFile = stats[i].isFile();
-            if (!isDir && !isFile) {
-                return null;
-            }
-
-            return {
-                path: path.join(subPath, filename),
-                isDir,
-                mimeType: mime.getType(path.join(dirPath, filename)),
-            };
+    const filesWithStats = await Promise.all(
+        files.map((filename) => {
+            return getFileOrDirInfo(path.join(subDirPath, filename));
         })
-        .filter(x => x);
+    );
 
-    return { files: filesWithStats, parentPath: subPath, depth };
+    return { files: filesWithStats.filter(x => x), parentPath: subDirPath, depth };
 };
 
-const sanitizePath = (path) => unescape(path.replace(/\.\./, ''));
+const sanitizePath = (path) => unescape(path.replace(/\.{2,}\//, ''));
 
 const getPathQueryParam = req => sanitizePath(req.query.path || '/')
 
-// CORS
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-    res.header("Access-Control-Allow-Methods", "DELETE,POST");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+const getDirectoryDepth = (subPath) => subPath === path.sep ? 0 : subPath.split(path.sep).length - 1;
 
-app.use(fileUpload());
+const getDirectoryAndParents = async function (req, res) {
+    try {
+        const requestedPath = getPathQueryParam(req);
 
+        const pathParts = requestedPath === path.sep ? [path.sep] : requestedPath.split(path.sep);
+
+        const topParentPath = req.query.withParentsTopParent ? sanitizePath(req.query.withParentsTopParent) : '/';
+        
+        // fetch directories up until a specific parent
+        const subPaths = pathParts
+            .map((_, index) => (pathParts.slice(0, index + 1).join(path.sep)) || '/')
+            .filter((p) => p.startsWith(topParentPath))
+
+        const topParentDepth = getDirectoryDepth(topParentPath);
+        const dirsOfFiles = await Promise.all(subPaths.map((subPath, i) => readDirFiles(subPath, topParentDepth + i)));
+
+        res.status(200).end(JSON.stringify(dirsOfFiles.filter(Boolean)));
+    } catch (e) {
+        console.log("ERROR while fetching dir with parents!", e);
+    }
+};
+
+// Routes
 app.get(
     '/directories',
     async function (req, res, next) {
         try {
             if (req.query.withParents === 'true') {
-                return next();
+                return getDirectoryAndParents(req, res);
             }
 
             const requestedPath = getPathQueryParam(req);
-            const dir = await readDirFiles(requestedPath, requestedPath.split(path.sep).length - 1);
+            const dir = await readDirFiles(requestedPath, getDirectoryDepth(requestedPath));
 
             if (!dir) {
                 res.status(404).end('Not found');
@@ -75,20 +109,6 @@ app.get(
             res.status(200).end(JSON.stringify(dir));
         } catch (e) {
             console.log("ERROR while fetching one dir!", e);
-        }
-    },
-    async function (req, res, next) {
-        try {
-            const requestedPath = getPathQueryParam(req);
-
-            const pathParts = requestedPath === path.sep ? [path.sep] : requestedPath.split(path.sep);
-            const subPaths = pathParts.map((_, index) => (pathParts.slice(0, index + 1).join(path.sep)) || '/');
-
-            const dirsOfFiles = await Promise.all(subPaths.map(readDirFiles));
-
-            res.status(200).end(JSON.stringify(dirsOfFiles.filter(Boolean)));
-        } catch (e) {
-            console.log("ERROR while fetching dir with parents!", e);
         }
     }
 );
@@ -115,6 +135,23 @@ const deleteFileHandler = (deleterFn) => async (req, res) => {
 app.delete('/directories', deleteFileHandler(f => fs.rmdir(f, {recursive: true})));
 
 app.delete('/files', deleteFileHandler(fs.unlink));
+
+const updateFile = async (req, res) => {
+    let { path } = req.body;
+    const { path: newPath } = req.body.updates;
+
+    // perform the requested updates, if any
+    if (newPath) {
+        await fs.rename(getFullPath(path), getFullPath(newPath));
+        path = newPath;
+    }
+
+    res.setHeader('Content-Type', 'application/json');
+    const fileInfo = await getFileOrDirInfo(path);
+    res.status(200).end(JSON.stringify(fileInfo));
+};
+app.put('/directories', updateFile);
+app.put('/files', updateFile);
 
 app.get('/files', async (req, res) => {
     const requestedPath = getPathQueryParam(req);
